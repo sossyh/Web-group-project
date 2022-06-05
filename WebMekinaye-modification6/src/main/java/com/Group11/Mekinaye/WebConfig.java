7f1055a20c7c197cf987f2f499cfef3333816aed
[jeal] <[jealsabb@gmail.com]>
2022-06-05 06:03:22 +0300

    final commit

--- WebMekinaye-modification6/src/main/java/com/Group11/Mekinaye/WebConfig.java
+++ WebMekinaye-modification6/src/main/java/com/Group11/Mekinaye/WebConfig.java
@@ -1,16 +1,16 @@
-package com.Group11.Mekinaye;
+// package com.Group11.Mekinaye;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/login").setViewName("login");
-    }
+//     @Override
+//     public void addViewControllers(ViewControllerRegistry registry) {
+//         registry.addViewController("/").setViewName("home");
+//         registry.addViewController("/login").setViewName("login");
+//     }
 
-}
+// }
